33d8676d630b486de66fe642500966ea532cd168
Guilherme Eras <135174494+GuilhermeRodriguesEras@users.noreply.github.com>
2026-08-31 18:07:02 +0000

    versão final

--- api/index.py
+++ api/index.py
@@ -450,7 +450,7 @@ def gerar_pdf_proposta(dados_front, dados_orcamento, contato, orcamento_id):
 
     pagamentos_html = (
         "<h1>Condições de pagamento:</h1><br/>"
-        f"<h2>Total do carrinho: <b> R$ {numero_pt(total_carrinho)} </b></h2><br/>"
+        f"<h2>Total: <b> R$ {numero_pt(total_carrinho)} </b></h2><br/>"
         f"<h2>Pagamento à vista com desconto: <b> R$ {numero_pt(valor_avista)}</b></h2><br/>"
         f"<h2><b>3x de R$ {numero_pt(valor_3x)}</b> sem juros</h2><br/>"
         f"<h2><b>12x de R$ {numero_pt(valor_12x)}</b> com juros no cartão. </h2><br/><br/>"
