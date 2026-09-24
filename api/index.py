bba18cc5238c142250ed68a3d9ed20f5ca8c5708
Guilherme Eras <135174494+GuilhermeRodriguesEras@users.noreply.github.com>
2026-09-24 16:35:53 +0000

    fix sinal

--- api/index.py
+++ api/index.py
@@ -3539,7 +3539,7 @@ def listarParaOSite():
         limite = datetime.strptime(data_limite, formato)
         atual = datetime.strptime(data_atual, formato)
 
-        return atual < limite
+        return atual >= limite
 
     data_inicio = request.args.get("data_inicio")
     data_fim = request.args.get("data_fim")
